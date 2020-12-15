a1edaa838efba0e6b08391919aa430f3e4d45ff8
Yongwoo Lee <jumong9@gmail.com>
2020-12-15 13:18:59 +0900

    .

--- resources/views/grade/payment/list.blade.php
+++ resources/views/grade/payment/list.blade.php
@@ -75,7 +75,7 @@
                             <td>{{ $list->class_order}}</td>
                             <td>{{ $list->main_yn == 0 ? '보조강사' : '주강사' }}</td>
                             <td>{{ number_format($list->lector_cost) }}</td>
-                            <td>{{ number_format($list->material_cost) }}</td>
+                            <td>{{ $list->main_yn == 1 ? number_format($list->material_cost) : 0 }}</td>
                             <td>{{ $list->class_status_value }}</td>
                             <td>{{ date_format($list->updated_at,'Y-m-d')}}</td>
                         @endforeach
